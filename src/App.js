51feeb46dcb55cf80785b77b0e58b1ba9a324373
Pedro <pedro_rga@hotmail.com>
2021-05-11 14:19:00 -0300

    react router navbar toggler

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Home from './components/HomeComponent'
 import Menu from './components/MenuComponent';
 import './App.css';
 import { DISHES } from './shared/dishes'
 import { Component } from 'react';
 import Header from './components/HeaderComponent'
 import Footer from './components/FooterComponent'
+import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
@@ -14,11 +16,20 @@ class App extends Component {
   }
 
   render() {
+    const HomePage = () => {
+      return (<Home />);
+    }
     return (
       <div className="App">
-        <Header/>
-        <Menu dishes={this.state.dishes} />
-        <Footer/>
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route path="/home" component={HomePage} />
+            <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+            <Redirect to="/home" />
+          </Switch>
+          <Footer />
+        </BrowserRouter>
       </div>
     );
   }
