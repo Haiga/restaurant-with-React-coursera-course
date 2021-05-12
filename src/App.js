4739fb5fdfc8accf906c540f5efae925a473a4df
Pedro <pedro_rga@hotmail.com>
2021-05-12 12:18:25 -0300

    more of Cards in a Single Page application

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import Home from './components/HomeComponent'
+import Contact from './components/ContactComponent'
 import Menu from './components/MenuComponent';
 import './App.css';
-import { DISHES } from './shared/dishes'
+import { DISHES } from './shared/dishes';
+import { COMMENTS } from './shared/comments';
+import { PROMOTIONS } from './shared/promotions';
+import { LEADERS } from './shared/leaders';
 import { Component } from 'react';
 import Header from './components/HeaderComponent'
 import Footer from './components/FooterComponent'
@@ -11,13 +15,19 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS
     };
   }
 
   render() {
     const HomePage = () => {
-      return (<Home />);
+      return (<Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+      promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+      leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+      />);
     }
     return (
       <div className="App">
@@ -26,6 +36,7 @@ class App extends Component {
           <Switch>
             <Route path="/home" component={HomePage} />
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+            <Route exact path="/contactus" component={Contact} />
             <Redirect to="/home" />
           </Switch>
           <Footer />
