2c00485cac36b299385dd09055c87bbebb53a8c2
Pedro <pedro_rga@hotmail.com>
2021-05-16 12:28:28 -0300

    combining reducers

---
+++ src/redux/promotions.js
@@ -0,0 +1,11 @@
+import { PROMOTIONS } from '../shared/dishes';
+
+
+export const Promotions = (state = PROMOTIONS, action) => {
+    switch (action.type) {
+        default:
+            return state;
+        default:
+            break;
+    }
+}
