caeee3bd8c573e0b2c30be297f00465120d66d9c
Pedro <pedro_rga@hotmail.com>
2021-05-24 12:06:45 -0300

    using POST manually

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -22,7 +22,7 @@ function RenderDish({ dish }) {
     )
 }
 
-function RenderComments({ comments, addComment, dishId }) {
+function RenderComments({ comments, postComment, dishId }) {
     if (comments != null) {
 
         let list = comments.map((comments) => {
@@ -45,7 +45,7 @@ function RenderComments({ comments, addComment, dishId }) {
                 <ul className="list-unstyled">
                     {list}
                 </ul>
-                <CommentForm dishId={dishId} addComment={addComment}>
+                <CommentForm dishId={dishId} postComment={postComment}>
 
                 </CommentForm>
             </div>
@@ -96,7 +96,7 @@ const DishDetail = (props) => {
                     <RenderDish dish={props.dish} />
                     <RenderComments
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         dishId={props.dish.id}
                     />
                 </div>
@@ -139,7 +139,7 @@ class CommentForm extends Component {
 
         console.log('Current State is: ' + JSON.stringify(values));
         //
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
 
     }
 
